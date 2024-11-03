fb36b9bbd8596e603274fc2b5d1206994d40fcc2
Michael Pohl <mpohl100@googlemail.com>
2024-11-03 16:43:21 +0100

    added first unit test for the neural network class

--- src/learn/NeuralNetwork.h
+++ src/learn/NeuralNetwork.h
@@ -3,25 +3,25 @@
 #include "Activation.h"
 #include "Layer.h"
 
-#include <vector>
 #include <memory>
+#include <vector>
 
 namespace learn {
 
 class NeuralNetwork {
-private:
-    std::vector<std::unique_ptr<Layer>> layers;
-    std::vector<std::unique_ptr<Activation>> activations;
-
 public:
-    void addLayer(std::unique_ptr<Layer> layer);
-    void addActivation(std::unique_ptr<Activation> activation);
+  void addActivationAndLayer(std::unique_ptr<Activation> activation,
+                             std::unique_ptr<Layer> layer);
 
-    std::vector<double> forward(const std::vector<double>& input);
-    void backward(const std::vector<double>& grad_output);
-    void train(const std::vector<std::vector<double>>& inputs,
-               const std::vector<std::vector<double>>& targets,
-               double learning_rate, int epochs);
+  std::vector<double> forward(const std::vector<double> &input);
+  void backward(const std::vector<double> &grad_output);
+  void train(const std::vector<std::vector<double>> &inputs,
+             const std::vector<std::vector<double>> &targets,
+             double learning_rate, int epochs);
+  int predict(const std::vector<double> &input);
+private:
+  std::vector<std::unique_ptr<Layer>> layers;
+  std::vector<std::unique_ptr<Activation>> activations;
 };
 
 } // namespace learn
