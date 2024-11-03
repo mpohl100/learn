37cc611dfc9152808ed117b7b5032ad500975b75
Michael Pohl <mpohl100@googlemail.com>
2024-11-03 19:09:26 +0100

    introduced Matrix to dense layer

--- src/learn/DenseLayer.h
+++ src/learn/DenseLayer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Layer.h"
+#include "matrix/Matrix.h"
 
 #include <random>
 #include <vector>
@@ -9,18 +10,18 @@ namespace learn {
 
 class DenseLayer : public Layer {
 public:
-    DenseLayer(int input_size, int output_size);
+  DenseLayer(int input_size, int output_size);
 
-    std::vector<double> forward(const std::vector<double>& input) override;
-    std::vector<double> backward(const std::vector<double>& grad_output) override;
-    void updateWeights(double learning_rate) override;
+  std::vector<double> forward(const std::vector<double> &input) override;
+  std::vector<double> backward(const std::vector<double> &grad_output) override;
+  void updateWeights(double learning_rate) override;
 
 private:
-    std::vector<std::vector<double>> weights;
-    std::vector<double> biases;
-    std::vector<double> input_cache;
-    std::vector<std::vector<double>> weight_grads;  // Gradients for weights
-    std::vector<double> bias_grads;                 // Gradients for biases
+  matrix::Matrix<double> weights;
+  std::vector<double> biases;
+  std::vector<double> input_cache;
+  matrix::Matrix<double> weight_grads; // Gradients for weights
+  std::vector<double> bias_grads;      // Gradients for biases
 };
 
 } // namespace learn
