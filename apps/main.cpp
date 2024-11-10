50c742fc0870c73cbef17f972aa0deef037ad5d3
Michael Pohl <mpohl100@googlemail.com>
2024-11-10 08:31:15 +0000

    make nn shape passable to a training session

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,12 +1,15 @@
+#include "learn/NeuralNetworkParams.h"
 #include "learn/TrainingSession.h"
 
 #include <clara.hpp>
 #include <iostream>
 
 struct DumbTrainingSession : learn::TrainingSession {
-  DumbTrainingSession() = default;
   virtual ~DumbTrainingSession() = default;
-  
+
+  DumbTrainingSession(const learn::TrainingParams &params)
+      : learn::TrainingSession{params}, _params{params} {}
+
   learn::TrainingSession::SessionData prepare_data() const override {
     // Initialize inputs and targets with zeros
     int num_samples = 1000; // Number of training samples
@@ -14,15 +17,17 @@ struct DumbTrainingSession : learn::TrainingSession {
     int num_classes =
         10; // Number of output classes (e.g., for digit classification)
 
-    std::vector<std::vector<double>> inputs(num_samples,
-                                            std::vector<double>(input_size, 0.0));
+    std::vector<std::vector<double>> inputs(
+        num_samples, std::vector<double>(input_size, 0.0));
     std::vector<std::vector<double>> targets(
         num_samples, std::vector<double>(num_classes, 0.0));
 
     return {inputs, targets};
   }
-};
 
+private:
+  learn::TrainingParams _params;
+};
 
 int main(int argc, char **argv) {
   using namespace clara;
@@ -43,7 +48,14 @@ int main(int argc, char **argv) {
 
   std::cout << "Hello, " << name << "!\n";
 
-  const auto training_session = DumbTrainingSession{};
+  const auto nn_shape = learn::NeuralNetworkShape{{
+      {784, 784, learn::LayerType::Dense, learn::ActivationType::ReLU},
+      {784, 128, learn::LayerType::Dense, learn::ActivationType::ReLU},
+      {128, 10, learn::LayerType::Dense, learn::ActivationType::Sigmoid},
+  }};
+  const auto training_params = learn::TrainingParams{nn_shape, 700, 300, 0.01, 10, 0.1};
+
+  const auto training_session = DumbTrainingSession{training_params};
   auto success_rate = training_session.train();
   std::cout << "Success rate: " << success_rate << '\n';
   return 0;
